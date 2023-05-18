fba8e98b6a3aaf6be1ba06e32604358d3146324f
Your Name <you@example.com>
2023-05-17 20:20:21 -0500

    added login handle bars and javascript

---
+++ seeds/seeds.js
@@ -0,0 +1,17 @@
+const sequlize = require('../config/connection');
+const { User } = require('../models');
+
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequlize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();

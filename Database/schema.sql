5c5b02398f9248a0b94c76260f0492a748a5677f
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-05-18 21:06:53 -0500

    Got rid of previous manager code that we weren't using

--- Database/schema.sql
+++ Database/schema.sql
@@ -22,14 +22,5 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE Manager (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  department_id INT NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  salary DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department (id)
-);
+
 

2a5d411b7de4d6983c09bdecbec34f287b1a1833
Your Name <you@example.com>
2023-05-18 19:10:25 -0500

    added employee and department routes

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,10 @@
-const sequlize = require('../config/connection');
-const { User } = require('../models');
+const sequlize = require('../Database/connection');
+const { User, Employee, Departments } = require('../models');
 
 const userData = require('./userData.json');
+const employeeData = require('./employeeData.json');
+const DepartmentData = require('/departmentData.json');
+const Department = require('../models/departments');
 
 const seedDatabase = async () => {
     await sequlize.sync({ force: true });
@@ -11,6 +14,18 @@ const seedDatabase = async () => {
         returning: true,
     });
 
+    for (const employee of employeeData) {
+        await Employee.create({
+            ...employee,
+            user_id: users[Math.florr(Math.random() * users.length)].id,
+        });
+    }
+    for (const department of departmentData) {
+        await Department.create({
+            ...Department,
+            user_id: users[Math.florr(Math.random() * users.length)].id,
+        });
+    }
     process.exit(0);
 };
 
